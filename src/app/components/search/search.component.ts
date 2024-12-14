cd73aeb4129c3592e9dea16e7b74a959f4aeddfa
Esraa_GamalH <esraa9gamal@gmail.com>
2024-12-15 01:14:28 +0200

    Implement search functionality

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
@@ -9,13 +9,45 @@ import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angul
   styleUrl: './search.component.css'
 })
 export class SearchComponent {
-
   searchForm = new FormGroup({
     cityName: new FormControl(''),
   });
+  
+  // filtered cities according to user input in search bar
+  @Input() forecasts!: any[];
+  cities: {cityName: string, cityId: number}[] = [];
+  filteredCities: {cityName: string, cityId: number}[] = [];
 
-  onSearch(){
-    console.log(this.searchForm.value['cityName']);
+  ngOnInit(){
+    // extract cities names from JSON data
+    this.cities = this.forecasts.map((data) => ({
+      cityName: data.city,
+      cityId: data.id,
+    }));
+    console.log("my cities", this.cities);
     
+
+    // listen to value changes in user input
+    this.searchForm.get('cityName')?.valueChanges.subscribe((value) => {
+      this.filteredCities = this.filterCities(value || '');
+    });
   }
+
+  // function to be called every filtering time
+  filterCities(query: string): { cityName: string; cityId: number }[] {
+    const lowerCaseQuery = query.toLowerCase();
+    if (lowerCaseQuery !== ''){
+      return this.cities.filter((city) =>
+        city.cityName.toLowerCase().includes(lowerCaseQuery)  // if the city includes the letters it will also appear
+      );
+    }
+    return [];
+  }
+
+    // Navigate to show city forecast details
+    navigateToCityDetails(cityId: number) {
+      
+      console.log('Selected city ID:', cityId);
+    }
+  
 }
