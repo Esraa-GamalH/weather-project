d67182cda0e1da29792875017469d59be3d47a7c
Esraa_GamalH <esraa9gamal@gmail.com>
2024-12-15 01:31:49 +0200

    add city forecast details page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,24 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { CityForecastComponent } from './pages/city-forecast/city-forecast.component';
 
 export const routes: Routes = [
+    // both / and /forecast paths navigate to landing page with cities list
     {
         path: '',
         component: HomeComponent,
         title: 'Home'
     },
+    {
+        path: 'forecast',
+        component: HomeComponent,
+        title: 'Home'
+    },
+    
+    // /cityForecast/:cityId navigates to city forecast details
+    {
+        path: 'cityForecast/:cityId',
+        component: CityForecastComponent,
+        title: 'Forecast Details'
+    }
 ];
---
+++ src/app/pages/city-forecast/city-forecast.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-city-forecast',
+  standalone: true,
+  imports: [],
+  templateUrl: './city-forecast.component.html',
+  styleUrl: './city-forecast.component.css'
+})
+export class CityForecastComponent {
+
+}
