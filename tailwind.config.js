342105c0b96bd590a0dca1d3cc13a32828b3a70a
Esraa_GamalH <esraa9gamal@gmail.com>
2024-12-14 22:28:18 +0200

    add color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
     "./node_modules/flowbite/**/*.js"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "orange": "#FB8500",
+        "yellow": "#FFB703",
+        "dark-blue": "#023047",
+        "blue": "#2E6F95",
+        "light-blue": "#8ECAE6"
+      },
+    },
   },
   plugins: [
     require('flowbite/plugin') // add flowbite library
